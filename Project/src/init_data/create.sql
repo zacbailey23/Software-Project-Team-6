bd26e764a9866bcaa77d7050abe97f1358c6b048
AndrewJ0508 <anjo6131@colorado.edu>
2023-11-12 16:27:01 -0700

    Created tables for cars and planner

--- Project/src/init_data/create.sql
+++ Project/src/init_data/create.sql
@@ -1,4 +1,20 @@
 CREATE TABLE users(
-                      username VARCHAR(50) PRIMARY KEY,
-                      password CHAR(60) NOT NULL
+    username VARCHAR(50) SERIAL PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
+
+CREATE TABLE cars(
+    year CHAR(12),
+    make VARCHAR(50),
+    model VARCHAR(50),
+    price DECIMAL,
+    car_id INT PRIMARY KEY
+);
+
+CREATE TABLE planner(
+    username VARCHAR(50),
+    description LONGTEXT,
+    date DATE,
+    time TIME,
+    cart_id FOREIGN KEY
 );
