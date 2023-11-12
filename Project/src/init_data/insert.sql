2653d45109970eccfab5b23f2dbcbe744a11c1d1
JoshSun777 <91907686+JoshSun777@users.noreply.github.com>
2023-11-12 16:56:24 -0700

    inserted some random car data

--- Project/src/init_data/insert.sql
+++ Project/src/init_data/insert.sql
@@ -1,1 +1,9 @@
 -- INSERT INTO users (username, password) VALUES ($1,$2);
+INSERT INTO cars(year, make, model, price, car_id)
+VALUES
+    (2020, 'Subaru', 'Outback', 200, 1),
+    (2019, 'Toyota', 'Pirus', 200, 2),
+    (2021, 'Toyota', 'Corolla', 200, 3),
+    (2017, 'Honda', 'Civic', 200, 4),
+    (2002, 'Honda', 'Accord', 200, 5);
+
