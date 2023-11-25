47246859f66cbf7a0cfe4be93b237a767fd6c50b
Zac Bailey <zaba2966@colorado.edu>
2023-11-25 14:34:59 -0700

    adding information to cart

--- Project/src/index.js
+++ Project/src/index.js
@@ -66,6 +66,8 @@ app.use(
 // *****************************************************
 // <!-- Section 4 : API Routes -->
 // *****************************************************
+
+
 function extractTopHotelsAndFlights(query, data) {
   const maxFlights = 20; // Move this constant outside of the sub-functions as it's used in multiple places
 
@@ -201,7 +203,7 @@ app.get('/search', async (req, res) => {
     templateData.flightsInfo = results;
   }
   // Render the EJS template with the extracted  data
-  res.render('pages/searchResults', { data: templateData });
+  res.render('pages/searchResults', { data: templateData, userLoggedIn: req.session.isLoggedIn || false});
 });
 
 
@@ -210,17 +212,585 @@ app.get('/welcome', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+
   res.redirect('/homepage');
 });
+
 app.get('/homepage', (req, res) => {
   // Check if the session and user exist
+  const flightInfoArray = [
+    {
+      flight_id: 1,
+      departureTime: "08:00:00",
+      departureLocation: "New York",
+      arrivalTime: "11:00:00",
+      arrivalLocation: "Los Angeles",
+      airline: "Delta Airlines",
+      departureAirport: "JFK",
+      arrivalAirport: "LAX",
+      departureCity: "New York",
+      arrivalCity: "Los Angeles",
+      totalMinimumFare: 250.00,
+      city: "New York",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 2,
+      departureTime: "10:30:00",
+      departureLocation: "Chicago",
+      arrivalTime: "14:15:00",
+      arrivalLocation: "Miami",
+      airline: "American Airlines",
+      departureAirport: "ORD",
+      arrivalAirport: "MIA",
+      departureCity: "Chicago",
+      arrivalCity: "Miami",
+      totalMinimumFare: 300.50,
+      city: "Chicago",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 3,
+      departureTime: "13:45:00",
+      departureLocation: "San Francisco",
+      arrivalTime: "16:30:00",
+      arrivalLocation: "Seattle",
+      airline: "United Airlines",
+      departureAirport: "SFO",
+      arrivalAirport: "SEA",
+      departureCity: "San Francisco",
+      arrivalCity: "Seattle",
+      totalMinimumFare: 180.75,
+      city: "San Francisco",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 4,
+      departureTime: "09:15:00",
+      departureLocation: "Dallas",
+      arrivalTime: "12:30:00",
+      arrivalLocation: "Denver",
+      airline: "Southwest Airlines",
+      departureAirport: "DFW",
+      arrivalAirport: "DEN",
+      departureCity: "Dallas",
+      arrivalCity: "Denver",
+      totalMinimumFare: 150.25,
+      city: "Dallas",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 5,
+      departureTime: "07:30:00",
+      departureLocation: "Los Angeles",
+      arrivalTime: "10:45:00",
+      arrivalLocation: "New York",
+      airline: "JetBlue Airways",
+      departureAirport: "LAX",
+      arrivalAirport: "JFK",
+      departureCity: "Los Angeles",
+      arrivalCity: "New York",
+      totalMinimumFare: 280.00,
+      city: "Los Angeles",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 6,
+      departureTime: "11:20:00",
+      departureLocation: "Miami",
+      arrivalTime: "15:05:00",
+      arrivalLocation: "Chicago",
+      airline: "American Airlines",
+      departureAirport: "MIA",
+      arrivalAirport: "ORD",
+      departureCity: "Miami",
+      arrivalCity: "Chicago",
+      totalMinimumFare: 320.75,
+      city: "Miami",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 7,
+      departureTime: "14:00:00",
+      departureLocation: "Seattle",
+      arrivalTime: "17:45:00",
+      arrivalLocation: "San Francisco",
+      airline: "United Airlines",
+      departureAirport: "SEA",
+      arrivalAirport: "SFO",
+      departureCity: "Seattle",
+      arrivalCity: "San Francisco",
+      totalMinimumFare: 190.50,
+      city: "Seattle",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 8,
+      departureTime: "08:45:00",
+      departureLocation: "Denver",
+      arrivalTime: "12:00:00",
+      arrivalLocation: "Dallas",
+      airline: "Southwest Airlines",
+      departureAirport: "DEN",
+      arrivalAirport: "DFW",
+      departureCity: "Denver",
+      arrivalCity: "Dallas",
+      totalMinimumFare: 160.75,
+      city: "Denver",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 9,
+      departureTime: "07:15:00",
+      departureLocation: "New York",
+      arrivalTime: "10:15:00",
+      arrivalLocation: "Los Angeles",
+      airline: "Delta Airlines",
+      departureAirport: "JFK",
+      arrivalAirport: "LAX",
+      departureCity: "New York",
+      arrivalCity: "Los Angeles",
+      totalMinimumFare: 265.50,
+      city: "New York",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 10,
+      departureTime: "09:45:00",
+      departureLocation: "Chicago",
+      arrivalTime: "13:30:00",
+      arrivalLocation: "Miami",
+      airline: "American Airlines",
+      departureAirport: "ORD",
+      arrivalAirport: "MIA",
+      departureCity: "Chicago",
+      arrivalCity: "Miami",
+      totalMinimumFare: 310.25,
+      city: "Chicago",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 11,
+      departureTime: "12:30:00",
+      departureLocation: "San Francisco",
+      arrivalTime: "15:15:00",
+      arrivalLocation: "Seattle",
+      airline: "United Airlines",
+      departureAirport: "SFO",
+      arrivalAirport: "SEA",
+      departureCity: "San Francisco",
+      arrivalCity: "Seattle",
+      totalMinimumFare: 195.00,
+      city: "San Francisco",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 12,
+      departureTime: "10:00:00",
+      departureLocation: "Dallas",
+      arrivalTime: "13:15:00",
+      arrivalLocation: "Denver",
+      airline: "Southwest Airlines",
+      departureAirport: "DFW",
+      arrivalAirport: "DEN",
+      departureCity: "Dallas",
+      arrivalCity: "Denver",
+      totalMinimumFare: 155.25,
+      city: "Dallas",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 13,
+      departureTime: "08:30:00",
+      departureLocation: "Los Angeles",
+      arrivalTime: "11:45:00",
+      arrivalLocation: "New York",
+      airline: "JetBlue Airways",
+      departureAirport: "LAX",
+      arrivalAirport: "JFK",
+      departureCity: "Los Angeles",
+      arrivalCity: "New York",
+      totalMinimumFare: 275.00,
+      city: "Los Angeles",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 14,
+      departureTime: "11:45:00",
+      departureLocation: "Miami",
+      arrivalTime: "15:30:00",
+      arrivalLocation: "Chicago",
+      airline: "American Airlines",
+      departureAirport: "MIA",
+      arrivalAirport: "ORD",
+      departureCity: "Miami",
+      arrivalCity: "Chicago",
+      totalMinimumFare: 315.75,
+      city: "Miami",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 15,
+      departureTime: "14:30:00",
+      departureLocation: "Seattle",
+      arrivalTime: "18:15:00",
+      arrivalLocation: "San Francisco",
+      airline: "United Airlines",
+      departureAirport: "SEA",
+      arrivalAirport: "SFO",
+      departureCity: "Seattle",
+      arrivalCity: "San Francisco",
+      totalMinimumFare: 200.50,
+      city: "Seattle",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 16,
+      departureTime: "09:45:00",
+      departureLocation: "Denver",
+      arrivalTime: "13:00:00",
+      arrivalLocation: "Dallas",
+      airline: "Southwest Airlines",
+      departureAirport: "DEN",
+      arrivalAirport: "DFW",
+      departureCity: "Denver",
+      arrivalCity: "Dallas",
+      totalMinimumFare: 165.75,
+      city: "Denver",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 17,
+      departureTime: "08:15:00",
+      departureLocation: "New York",
+      arrivalTime: "11:15:00",
+      arrivalLocation: "Los Angeles",
+      airline: "Delta Airlines",
+      departureAirport: "JFK",
+      arrivalAirport: "LAX",
+      departureCity: "New York",
+      arrivalCity: "Los Angeles",
+      totalMinimumFare: 260.50,
+      city: "New York",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 18,
+      departureTime: "10:30:00",
+      departureLocation: "Chicago",
+      arrivalTime: "14:15:00",
+      arrivalLocation: "Miami",
+      airline: "American Airlines",
+      departureAirport: "ORD",
+      arrivalAirport: "MIA",
+      departureCity: "Chicago",
+      arrivalCity: "Miami",
+      totalMinimumFare: 305.25,
+      city: "Chicago",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 19,
+      departureTime: "13:15:00",
+      departureLocation: "San Francisco",
+      arrivalTime: "16:00:00",
+      arrivalLocation: "Seattle",
+      airline: "United Airlines",
+      departureAirport: "SFO",
+      arrivalAirport: "SEA",
+      departureCity: "San Francisco",
+      arrivalCity: "Seattle",
+      totalMinimumFare: 185.00,
+      city: "San Francisco",
+      numberOfConnections: 0,
+    },
+    {
+      flight_id: 20,
+      departureTime: "11:00:00",
+      departureLocation: "Dallas",
+      arrivalTime: "14:15:00",
+      arrivalLocation: "Denver",
+      airline: "Southwest Airlines",
+      departureAirport: "DFW",
+      arrivalAirport: "DEN",
+      departureCity: "Dallas",
+      arrivalCity: "Denver",
+      totalMinimumFare: 155.50,
+      city: "Dallas",
+      numberOfConnections: 0,
+    },
+  ];
+  const hotelInfoArray = [
+    {
+      id: 1,
+      name: "Grand Hyatt New York",
+      areaName: "Midtown Manhattan",
+      starRating: 4,
+      address: {
+        cityName: "New York",
+        addressLineOne: "109 E 42nd St",
+        stateCode: "NY",
+        countryCode: "US",
+        zip: "10017"
+      },
+    },
+    {
+      id: 2,
+      name: "Fontainebleau Miami Beach",
+      areaName: "Miami Beach",
+      starRating: 5,
+      address: {
+        cityName: "Miami Beach",
+        addressLineOne: "4441 Collins Ave",
+        stateCode: "FL",
+        countryCode: "US",
+        zip: "33140"
+      },
+    },
+    {
+      id: 3,
+      name: "The Ritz-Carlton, Chicago",
+      areaName: "Magnificent Mile",
+      starRating: 4,
+      address: {
+        cityName: "Chicago",
+        addressLineOne: "160 E Pearson St",
+        stateCode: "IL",
+        countryCode: "US",
+        zip: "60611"
+      },
+    },
+    {
+      id: 4,
+      name: "The Ritz-Carlton, Denver",
+      areaName: "Downtown Denver",
+      starRating: 4,
+      address: {
+        cityName: "Denver",
+        addressLineOne: "1881 Curtis St",
+        stateCode: "CO",
+        countryCode: "US",
+        zip: "80202"
+      },
+    },
+    {
+      id: 5,
+      name: "The Ritz-Carlton, Los Angeles",
+      areaName: "Downtown Los Angeles",
+      starRating: 4,
+      address: {
+        cityName: "Los Angeles",
+        addressLineOne: "900 W Olympic Blvd",
+        stateCode: "CA",
+        countryCode: "US",
+        zip: "90015"
+      },
+    },
+    {
+      id: 6,
+      name: "Fairmont San Francisco",
+      areaName: "Nob Hill",
+      starRating: 5,
+      address: {
+        cityName: "San Francisco",
+        addressLineOne: "950 Mason St",
+        stateCode: "CA",
+        countryCode: "US",
+        zip: "94108"
+      },
+    },
+    {
+      id: 7,
+      name: "The Joule, Dallas",
+      areaName: "Downtown Dallas",
+      starRating: 4,
+      address: {
+        cityName: "Dallas",
+        addressLineOne: "1530 Main St",
+        stateCode: "TX",
+        countryCode: "US",
+        zip: "75201"
+      },
+    },
+    {
+      id: 8,
+      name: "The Confidante Miami Beach",
+      areaName: "Miami Beach",
+      starRating: 4,
+      address: {
+        cityName: "Miami Beach",
+        addressLineOne: "4041 Collins Ave",
+        stateCode: "FL",
+        countryCode: "US",
+        zip: "33140"
+      },
+    },
+    {
+      id: 9,
+      name: "The Langham, New York, Fifth Avenue",
+      areaName: "Midtown Manhattan",
+      starRating: 5,
+      address: {
+        cityName: "New York",
+        addressLineOne: "400 5th Ave",
+        stateCode: "NY",
+        countryCode: "US",
+        zip: "10018"
+      },
+    },
+    {
+      id: 10,
+      name: "The Ritz-Carlton, Denver",
+      areaName: "Downtown Denver",
+      starRating: 4,
+      address: {
+        cityName: "Denver",
+        addressLineOne: "1881 Curtis St",
+        stateCode: "CO",
+        countryCode: "US",
+        zip: "80202"
+      },
+    },
+    {
+      id: 11,
+      name: "The Langham, Chicago",
+      areaName: "River North",
+      starRating: 4,
+      address: {
+        cityName: "Chicago",
+        addressLineOne: "330 N Wabash Ave",
+        stateCode: "IL",
+        countryCode: "US",
+        zip: "60611"
+      },
+    },
+    {
+      id: 12,
+      name: "Four Seasons Hotel Miami",
+      areaName: "Brickell",
+      starRating: 5,
+      address: {
+        cityName: "Miami",
+        addressLineOne: "1435 Brickell Ave",
+        stateCode: "FL",
+        countryCode: "US",
+        zip: "33131"
+      },
+    },
+    {
+      id: 13,
+      name: "The Westin Bonaventure Hotel & Suites, Los Angeles",
+      areaName: "Downtown Los Angeles",
+      starRating: 4,
+      address: {
+        cityName: "Los Angeles",
+        addressLineOne: "404 S Figueroa St",
+        stateCode: "CA",
+        countryCode: "US",
+        zip: "90071"
+      },
+    },
+    {
+      id: 14,
+      name: "Hyatt Regency San Francisco",
+      areaName: "Embarcadero",
+      starRating: 4,
+      address: {
+        cityName: "San Francisco",
+        addressLineOne: "5 Embarcadero Center",
+        stateCode: "CA",
+        countryCode: "US",
+        zip: "94111"
+      },
+    },
+    {
+      id: 15,
+      name: "The Adolphus, Autograph Collection",
+      areaName: "Downtown Dallas",
+      starRating: 4,
+      address: {
+        cityName: "Dallas",
+        addressLineOne: "1321 Commerce St",
+        stateCode: "TX",
+        countryCode: "US",
+        zip: "75202"
+      },
+    },
+    {
+      id: 16,
+      name: "Eden Roc Miami Beach",
+      areaName: "Miami Beach",
+      starRating: 4,
+      address: {
+        cityName: "Miami Beach",
+        addressLineOne: "4525 Collins Ave",
+        stateCode: "FL",
+        countryCode: "US",
+        zip: "33140"
+      },
+    },
+    {
+      id: 17,
+      name: "The Langham, New York, Fifth Avenue",
+      areaName: "Midtown Manhattan",
+      starRating: 5,
+      address: {
+        cityName: "New York",
+        addressLineOne: "400 5th Ave",
+        stateCode: "NY",
+        countryCode: "US",
+        zip: "10018"
+      },
+    },
+    {
+      id: 18,
+      name: "The Ritz-Carlton, Denver",
+      areaName: "Downtown Denver",
+      starRating: 4,
+      address: {
+        cityName: "Denver",
+        addressLineOne: "1881 Curtis St",
+        stateCode: "CO",
+        countryCode: "US",
+        zip: "80202"
+      },
+    },
+    {
+      id: 19,
+      name: "The Langham, Chicago",
+      areaName: "River North",
+      starRating: 4,
+      address: {
+        cityName: "Chicago",
+        addressLineOne: "330 N Wabash Ave",
+        stateCode: "IL",
+        countryCode: "US",
+        zip: "60611"
+      },
+    },
+    {
+      id: 20,
+      name: "Four Seasons Hotel Denver",
+      areaName: "Downtown Denver",
+      starRating: 5,
+      address: {
+        cityName: "Denver",
+        addressLineOne: "1111 14th St",
+        stateCode: "CO",
+        countryCode: "US",
+        zip: "80202"
+      },
+    },
+  ];
+  
   if(req.session && req.session.user) {
-    res.render('pages/homepage', { user: req.session.user });
+    res.render('pages/homepage', { user: req.session.user, flights: flightInfoArray || [], hotels: hotelInfoArray || [] });
   } else {
     // Handle the case where the session or user is not set
-    res.render('pages/homepage', { user: null });
+    res.render('pages/homepage', { user: null, flights: flightInfoArray, hotels: hotelInfoArray});
   }
 });
+
 app.get('/register', (req, res) => {
   res.render('pages/register');
 });
@@ -278,6 +848,7 @@ app.post('/login', async (req, res) => {
       res.render('pages/login', { error: 'Incorrect username or password.' });
       return;
     }
+    req.session.isLoggedIn = true;
     req.session.user = user;
     req.session.save();
 
@@ -302,8 +873,69 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/cartItem", (req, res) => {
-  res.render('pages/cart');
+  if(req.session && req.session.user) {
+    res.render('pages/cart', { user: req.session.user});
+  } else {
+    // Handle the case where the session or user is not set
+    res.render('pages/cart', { user: null});
+  }
 });
+
+app.post('/submitFlightData', async (req, res) => {
+  try {
+      const flightData = JSON.parse(req.body.flightData);
+      
+      // Insert data into flightsReturned table
+      const flightInsertQuery = 'INSERT INTO flightsReturned (departureTime, departureLocation, arrivalTime, arrivalLocation, airline, departureAirport, arrivalAirport, departureCity, arrivalCity, totalMinimumFare, city, numberOfConnections) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING flight_id;';
+      const flightValues = [flightData.departureTime, flightData.departureLocation, flightData.arrivalTime, flightData.arrivalLocation, flightData.airline, flightData.departureAirport, flightData.arrivalAirport, flightData.departureCity, flightData.arrivalCity, flightData.totalMinimumFare, flightData.city, flightData.numberOfConnections];
+      
+      // const flightInsertResult = await db.one(flightInsertQuery, flightValues);
+      
+      // const userId = /* Retrieve user id here */;
+      // const flightId = flightInsertResult.flight_id;
+
+      // // Insert data into cartItem table
+      // const cartInsertQuery = 'INSERT INTO cartItem (user_id, flight_id) VALUES ($1, $2);';
+      // const cartValues = [userId, flightId];
+      // await db.none(cartInsertQuery, cartValues);
+
+      res.send('Flight data submitted successfully');
+  } catch (err) {
+      console.error('Error in submitting flight data', err);
+      res.status(500).send('Error in submitting flight data');
+  }
+});
+
+// Similar changes should be made to /submitHotelData
+
+app.post('/submitHotelData', async (req, res) => {
+  try {
+      const hotelData = JSON.parse(req.body.hotelData);
+      
+      // Extract address details from the nested structure
+      const { cityName, addressLineOne, stateCode, countryCode, zip } = hotelData.address;
+
+      // Insert data into hotels table
+      const hotelInsertQuery = 'INSERT INTO hotels (name, areaName, starRating, addressLineOne, cityName, stateCode, countryCode, zip, availableRooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING hotel_id;';
+      const hotelValues = [hotelData.name, hotelData.areaName, hotelData.starRating, addressLineOne, cityName, stateCode, countryCode, zip];
+      const hotelInsertResult = await db.one(hotelInsertQuery, hotelValues);
+      
+      // const userId = /* Retrieve user id here */;
+      // const hotelId = hotelInsertResult.hotel_id;
+
+      // // Insert data into cartItem table
+      // const cartInsertQuery = 'INSERT INTO cartItem (user_id, hotel_id) VALUES ($1, $2);';
+      // const cartValues = [userId, hotelId];
+      // await db.none(cartInsertQuery, cartValues);
+
+      res.send('Hotel data submitted successfully');
+  } catch (err) {
+      console.error('Error in submitting hotel data', err);
+      res.status(500).send('Error in submitting hotel data');
+  }
+});
+
+
 
 app.post("/cartItem/add", async (req, res) => {
   const item_id = parseInt(req.body.item_id);
